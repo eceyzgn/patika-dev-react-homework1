ec53b3d2998624e40ba8cccf8533b4c8d317dd15
eceyzgn <ece@anv.com.tr>
2022-04-09 19:31:30 +0300

    adding

---
+++ app.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+
+async function getData(user_id){
+    const {data:user}= await axios ("https://jsonplaceholder.typicode.com/users/"+user_id);
+    console.log(user);
+    const {data:post} = await axios(`https://jsonplaceholder.typicode.com/posts?userId=`+user_id);
+    console.log(post)
+}
+
+export default getData;
